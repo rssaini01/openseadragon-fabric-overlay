d970913c49416e803c664ca24f5829238f51ce87
Ravi Shankar Saini <sainiravi015@gmail.com>
2025-09-21 12:51:09 +0530

    added demo site

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -2,14 +2,20 @@ import { defineConfig } from "vite";
 import path from 'path';
 
 export default defineConfig(({ mode }) => ({
-  root: "./demo",
-  base: mode === "prod" ? "/openseadragon-fabric/" : "/",
+  root: path.resolve(__dirname),
   resolve: {
     alias: {
       "openseadragon-fabric-overlay":
         mode === "prod"
-          ? path.resolve(__dirname, "../dist/fabric-overlay.ts")
+          ? path.resolve(__dirname, "../dist/fabric-overlay.js")
           : path.resolve(__dirname, "../src/fabric-overlay.ts"),
     }
   },
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
+    },
+  }
 }));
