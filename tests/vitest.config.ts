93c4ba1df669aea5cf57e9d5d42e5425dde46746
Ravi Shankar Saini <sainiravi015@gmail.com>
2025-12-29 11:38:25 +0530

    Add Codecov bundler plugin

--- tests/vitest.config.ts
+++ tests/vitest.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vitest/config';
+import { codecovVitePlugin } from '@codecov/vite-plugin';
 
 export default defineConfig({
-    test: {
-        environment: 'jsdom',
-        globals: true,
-        coverage: {
-            reporter: ['text', 'json-summary', 'json'],
-        },
-        reporters: ['default', 'junit'],
-        outputFile: '../test-results.xml',
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    coverage: {
+      reporter: ['text', 'json-summary', 'json'],
     },
+    reporters: ['default', 'junit'],
+    outputFile: '../test-results.xml',
+  },
+  plugins: [
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "openseadragon-fabric-overlay",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
 });
